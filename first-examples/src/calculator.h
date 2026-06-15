2c0abd2a4d2e00b2dd819f9ae0dec542b8ba7d80
slemanz <william_sleman@hotmail.com>
2026-06-15 18:21:15 -0300

    first-examples(feat): add calculator header

---
+++ first-examples/src/calculator.h
@@ -0,0 +1,7 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+/* Multiplies two integers, saturating to INT_MAX when the result overflows. */
+int Calculator_Multiply(int a, int b);
+
+#endif /* CALCULATOR_H */
